760fb7873edb51c40b66634f64e2803a8c39cb86
波波老师V587 <56994865+BOBOLAOSHIV587@users.noreply.github.com>
2026-01-30 02:46:54 +0800

    Airbrush解锁Premium

---
+++ JS/Airbrush/JS/Airbrush.js
@@ -0,0 +1,59 @@
+body = {
+  "code": 0,
+  "message": "success",
+  "data": {
+    "active_sub_type": 2,
+    "account_type": 2,
+    "sub_type_name": "续期",
+    "active_sub_order_id": "7422540573779191911",
+    "trial_period_invalid_time": "1770276372269",
+    "show_renew_flag_abroad": false,
+    "current_order_invalid_time": "1770276372269",
+    "active_order_id": "7422540573649168483",
+    "valid_contract_info": [
+      {
+        "product_id": "7396024776856721271",
+        "sub_period": 3,
+        "valid_time": "4102444799000",
+        "platform": 2,
+        "contract_status": 2,
+        "contract_id": "7422540573024217181",
+        "pay_channel": "iap",
+        "invalid_time": "1770276372000",
+        "sub_period_duration": 1
+      }
+    ],
+    "limit_type": 0,
+    "active_sub_type_name": "续期",
+    "use_vip": true,
+    "have_valid_contract": true,
+    "in_grace_period": false,
+    "derive_type_name": "普通会员",
+    "derive_type": 1,
+    "in_trial_period": false,
+    "active_third_product_id": "com.meitu.airbrush.autorenew.vip1",
+    "is_vip": true,
+    "membership": {
+      "id": "3001",
+      "display_name": "AirBrush VIP",
+      "level": 1,
+      "level_name": "普通会员"
+    },
+    "active_promotion_status_list": [
+      2
+    ],
+    "sub_type": 2,
+    "account_id": "2659055011",
+    "invalid_time": "1770276372269",
+    "valid_time": "4102444799000",
+    "active_product_id": "7396024776856721271",
+    "active_sub_period": 3,
+    "show_renew_flag": true,
+    "active_sub_period_duration": 1,
+    "active_promotion_status": 2
+  },
+  "request_id": "48f0b2f41a2d490faddaa373dd1ee313",
+  "success": true,
+  "error_code": "00000"
+}
+$done({body: JSON.stringify(body)});
